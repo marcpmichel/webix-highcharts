f01d79e0f59c787e116d51b43d06d3a62e6affa3
marc.michel@hespul.org <marc.michel@hespul.org>
2015-08-18 17:47:15 +0200

    refactored updateSeries + defaults

--- webix-highcharts.js
+++ webix-highcharts.js
@@ -4,10 +4,7 @@
 
 webix.protoUI({ name:"highcharts",
 
-    updateSeries: function() {
-        // remove previous series
-        while(this.chart.series.length > 0) { this.chart.series[0].remove(false); }
-
+    addSeries: function() {
         // add new series
         this.data.each( function(series) { this.chart.addSeries( series, false, false ); }, this );
         this.chart.redraw();
@@ -15,6 +12,8 @@ webix.protoUI({ name:"highcharts",
 
     reset: function() {
         this.data.clearAll();
+        // remove previous series
+        while(this.chart.series.length > 0) { this.chart.series[0].remove(false); }
     },
 
     setParams: function(params) {
@@ -26,36 +25,30 @@ webix.protoUI({ name:"highcharts",
       return this.chart.options;
     },
 
+    defaults: {
+          chart:{ renderTo: null },
+          plotOptions: { series: { animation: false } }
+    },
+
     $init:function(config){
 
         this._autoreset = (config.autoreset === undefined) ? true : config.autoreset;
-
-        // need some kind of deep merge
-        if(!config.chart) config.chart = {};
         config.chart.renderTo = this.$view;
-        if(!config.plotOptions) config.plotOptions = {};
-        if(!config.plotOptions.series) config.plotOptions.series = {};
-        config.plotOptions.series.animation = false;
 
         this.chart = new Highcharts.Chart(config);
         this.$ready.push(this._after_init_call);
     },
 
-    defaults: {
-          chart:{ renderTo: this.$view },
-          plotOptions: { series: { animation: false } }
-    },
-
     _after_init_call: function() {
-        this.data.attachEvent("onStoreUpdated", webix.bind(this.updateSeries, this) );
+        this.data.attachEvent("onStoreUpdated", webix.bind(this.addSeries, this) );
         if(this._autoreset) {
             this.attachEvent("onBeforeLoad", webix.bind(this.reset, this) );
         }
         if(this.config.data) { this.parse(this.config.data);  }
     },
 
-    $setSize: function() {
-        webix.ui.view.prototype.$setSize.apply(this, arguments);
+    $setSize: function(x,y) {
+        webix.ui.view.prototype.$setSize.call(this, x, y);
         if(this.chart) this.chart.reflow();
     }
 
